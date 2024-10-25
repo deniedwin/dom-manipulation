deb08f939cf857b6405fc602ba8e2984fefafda7
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 15:29:27 +0200

    Experimented with js DOM manipulation
    Use for each loop to generate multiple buttons

--- script.js
+++ script.js
@@ -31,10 +31,62 @@ container.appendChild(twoContent);
 // const container2 = document.querySelector("#twoContent");
 const head1 = document.createElement("h1");
 head1.classList.add("head1");
-head1.textContent = "I'm in a div";
+head1.textContent = "inside a div";
 twoContent.appendChild(head1);
 
 const para2 = document.createElement("p");
 para2.classList.add("para2");
-para2.textContent = "ME TOO!";
+para2.textContent = "this one as well";
 twoContent.appendChild(para2);
+
+// //button
+// //method 1 is in html file index.html
+
+// //method 2
+// const btn2 = document.querySelector("#btn2");
+// btn2.onclick = () => alert("Hello World 2");
+
+// //method 3
+// const btn3 = document.querySelector("#btn3");
+// btn3.addEventListener("click", () => {
+//   alert("Hello World 3");
+// });
+
+
+// //how to add multiple iterations of functions as js to the html
+// function alertFunction() {
+//     alert("YAY! YOU DID IT!");
+// }
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", alertFunction);
+
+// btn.addEventListener("click", function (e) {
+//     console.log(e);
+// });
+
+// btn.addEventListener("click", function (e) {
+//     console.log(e.target);
+// });
+
+// btn.addEventListener("click", function (e) {
+//     e.target.style.background = "blue";
+// });
+
+
+//how to add mutiple buttons to the DOM
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+  
+  
+
+
