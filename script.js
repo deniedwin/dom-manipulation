71b65ff30a3946930f17b5c9c5a4c15ea8073be0
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 11:56:38 +0200

    Create paragraph and h3 with js DOM manipulation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // your JavaScript file
-const container = document.querySelector("#container");
+const container = document.querySelector("#container"); //reference to container that already exists
+const content = document.createElement("div"); //create a new div and store it to content
+content.classList.add("content"); //add class "content"
+content.textContent = "This is the glorious text-content!"; //add text to class "content"
+container.appendChild(content); //append/attach this class to container
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+//create a paragraph with red text
+const para = document.createElement("p");
+para.classList.add("para");
+para.textContent = "hey I'm red!";
+para.style.color = "red";
+container.appendChild(para);
 
-container.appendChild(content);
+//create a h3 with blue text
+const head3 = document.createElement("h3");
+head3.classList.add("head3");
+head3.textContent = "I'm a blue h3!";
+head3.style.color = "blue";
+container.appendChild(head3);
