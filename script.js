b0c2df10798ab0b137e0b849e0e9def6572f35f4
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 12:29:01 +0200

    Completed 1st exercise DOM manipulation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // your JavaScript file
 const container = document.querySelector("#container"); //reference to container that already exists
 const content = document.createElement("div"); //create a new div and store it to content
-content.classList.add("content"); //add class "content"
+content.classList.add("content"); //add class "content" to the list
 content.textContent = "This is the glorious text-content!"; //add text to class "content"
 container.appendChild(content); //append/attach this class to container
 
@@ -18,3 +18,23 @@ head3.classList.add("head3");
 head3.textContent = "I'm a blue h3!";
 head3.style.color = "blue";
 container.appendChild(head3);
+
+//create a div with h1 and p inside it
+const twoContent = document.createElement("div");
+twoContent.classList.add("twoContent");
+twoContent.style.borderStyle = "solid";
+twoContent.style.borderColor = "black";
+twoContent.style.backgroundColor = "pink";
+container.appendChild(twoContent);
+
+//create h1 and p
+// const container2 = document.querySelector("#twoContent");
+const head1 = document.createElement("h1");
+head1.classList.add("head1");
+head1.textContent = "I'm in a div";
+twoContent.appendChild(head1);
+
+const para2 = document.createElement("p");
+para2.classList.add("para2");
+para2.textContent = "ME TOO!";
+twoContent.appendChild(para2);
